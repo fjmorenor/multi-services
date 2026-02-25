bbaeead34415015eefa392f3e538cbaaa17982fb
Francisco <fmorenoreina@gmail.com>
2026-02-25 17:39:03 +0100

    fix: corregida sintaxis de variables y referencias en networking

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -35,13 +35,13 @@ resource "google_compute_router" "router" {
 resource "google_compute_router_nat" "nat" {
     name = "${var.network_name}-nat"
     router = google_compute_router.router.name
-    nat_ip_allocate_option =" AUTO_ONLY"
+    nat_ip_allocate_option ="AUTO_ONLY"
     source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
 resource "google_compute_firewall" "firewall" {
     name = "firewall"
-    network = google_compute_network.vpc
+    network = google_compute_network.vpc.name
     source_ranges = [ "130.211.0.0/22", "35.191.0.0/16" ]
     project = var.project_id
     allow {
